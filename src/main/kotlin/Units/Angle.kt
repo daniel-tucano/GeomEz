2fbb8426da2433fa2c9a2bfbf9c3a2a42a3ab1f9
Daniel Ribeiro Santiago <daniel.santiago@zup.com.br>
2021-08-08 00:28:27 -0300

    Modify Angle class to declare abstract operations which inside classes override

--- src/main/kotlin/Units/Angle.kt
+++ src/main/kotlin/Units/Angle.kt
@@ -6,36 +6,46 @@ import kotlin.math.PI
 sealed class Angle {
     abstract val value: Double
 
+    abstract operator fun plus(value: Double): Angle
+    abstract operator fun minus(value: Double): Angle
+    abstract operator fun times(value: Double): Angle
+    abstract operator fun div(value: Double): Angle
+
+    abstract operator fun plus(angle: Angle): Angle
+    abstract operator fun minus(angle: Angle): Angle
+    abstract operator fun times(angle: Angle): Angle
+    abstract operator fun div(angle: Angle): Angle
+
     data class Radians (override var value: Double): Angle() {
         fun toDegrees(): Degrees = Degrees(this.value * 180.0/PI)
 
-        operator fun plus(value: Double): Double = this.value + value
-        operator fun minus(value: Double): Double = this.value - value
-        operator fun times(value: Double): Double = this.value * value
-        operator fun div(value: Double): Double = this.value / value
+        override operator fun plus(value: Double): Radians = Radians(this.value + value)
+        override operator fun minus(value: Double): Radians = Radians(this.value - value)
+        override operator fun times(value: Double): Radians = Radians(this.value * value)
+        override operator fun div(value: Double): Radians = Radians(this.value / value)
 
-        operator fun plus(angle: Angle): Radians {
+        override operator fun plus(angle: Angle): Radians {
             return when (angle) {
                 is Radians -> Radians(this.value + angle.value)
                 is Degrees -> Radians(this.value + angle.value * PI/180)
             }
         }
 
-        operator fun minus(angle: Angle): Angle {
+        override operator fun minus(angle: Angle): Radians {
             return when (angle) {
                 is Radians -> Radians(this.value - angle.value)
                 is Degrees -> Radians(this.value - angle.value * PI/180)
             }
         }
 
-        operator fun times(angle: Angle): Angle {
+        override operator fun times(angle: Angle): Radians {
             return when (angle) {
                 is Radians -> Radians(this.value * angle.value)
                 is Degrees -> Radians(this.value * angle.value * PI/180)
             }
         }
 
-        operator fun div(angle: Angle): Angle {
+        override operator fun div(angle: Angle): Radians {
             return when (angle) {
                 is Radians -> Radians(this.value * angle.value)
                 is Degrees -> Radians(this.value * angle.value * PI/180)
@@ -45,36 +55,36 @@ sealed class Angle {
     data class Degrees (override var value: Double): Angle() {
         fun toRadians(): Radians = Radians(this.value * PI/180.0)
 
-        operator fun plus(value: Double): Double = this.value + value
-        operator fun minus(value: Double): Double = this.value - value
-        operator fun times(value: Double): Double = this.value * value
-        operator fun div(value: Double): Double = this.value / value
+        override operator fun plus(value: Double): Degrees = Degrees(this.value + value)
+        override operator fun minus(value: Double): Degrees = Degrees(this.value - value)
+        override operator fun times(value: Double): Degrees = Degrees(this.value * value)
+        override operator fun div(value: Double): Degrees = Degrees(this.value / value)
 
-        operator fun plus(angle: Angle): Angle {
+        override operator fun plus(angle: Angle): Degrees {
             return when (angle) {
-                is Radians -> Radians(this.value + angle.value * 180/PI)
-                is Degrees -> Radians(this.value + angle.value)
+                is Radians -> Degrees(this.value + angle.value * 180/PI)
+                is Degrees -> Degrees(this.value + angle.value)
             }
         }
 
-        operator fun minus(angle: Angle): Angle {
+        override operator fun minus(angle: Angle): Degrees {
             return when (angle) {
-                is Radians -> Radians(this.value + angle.value * 180/PI)
-                is Degrees -> Radians(this.value + angle.value)
+                is Radians -> Degrees(this.value + angle.value * 180/PI)
+                is Degrees -> Degrees(this.value + angle.value)
             }
         }
 
-        operator fun times(angle: Angle): Angle {
+        override operator fun times(angle: Angle): Degrees {
             return when (angle) {
-                is Radians -> Radians(this.value + angle.value * 180/PI)
-                is Degrees -> Radians(this.value + angle.value)
+                is Radians -> Degrees(this.value + angle.value * 180/PI)
+                is Degrees -> Degrees(this.value + angle.value)
             }
         }
 
-        operator fun div(angle: Angle): Angle {
+        override operator fun div(angle: Angle): Degrees {
             return when (angle) {
-                is Radians -> Radians(this.value + angle.value * 180/PI)
-                is Degrees -> Radians(this.value + angle.value)
+                is Radians -> Degrees(this.value + angle.value * 180/PI)
+                is Degrees -> Degrees(this.value + angle.value)
             }
         }
     }
