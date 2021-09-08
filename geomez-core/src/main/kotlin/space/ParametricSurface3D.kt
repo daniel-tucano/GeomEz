210edfb380612b5d50dc0673c1d28a6772c9769e
Daniel Ribeiro Santiago <danielribeirosantiago.tucano@gmail.com>
2021-09-08 20:44:45 -0300

    Creates a ParametricSurface3D class that represents parametric surfaces

---
+++ geomez-core/src/main/kotlin/space/ParametricSurface3D.kt
@@ -0,0 +1,16 @@
+package space
+
+import space.elements.Point3D
+import space.functions.Function3D
+
+class ParametricSurface3D (
+    val xParametricFunction: Function3D,
+    val yParametricFunction: Function3D,
+    val zParametricFunction: Function3D
+) {
+
+
+    fun invoke(x: Double, y: Double): Point3D {
+        return Point3D(xParametricFunction(x,y), yParametricFunction(x,y), zParametricFunction(x,y))
+    }
+}
