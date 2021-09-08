5c56ab253302ed3f1cdecdd54077c6be5a9bd13e
Daniel Ribeiro Santiago <danielribeirosantiago.tucano@gmail.com>
2021-09-08 20:45:16 -0300

    Changes build.gradle to be able to publish package

--- geomez-core/build.gradle.kts
+++ geomez-core/build.gradle.kts
@@ -1,14 +1,84 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
+    `java-library`
+    `maven-publish`
+    signing
     kotlin("jvm") version "1.5.21"
 }
 
-group = "me.daanr"
-version = "1.0-SNAPSHOT"
+group = "io.github.daniel-tucano"
+version = "0.1.2-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+
+publishing {
+    publications {
+        create<MavenPublication>("geomez-core-mavenKotlin") {
+            artifactId = "geomez-core"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name.set("geomez-core")
+                description.set("An easy way to build geometric elements")
+                url.set("https://github.com/daniel-tucano/GeomEz")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("daniel-tucano")
+                        name.set("Daniel Ribeiro Santiago")
+                        email.set("daanrsantiago@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/daniel-tucano/GeomEz")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = URI("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials {
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["geomez-core-mavenKotlin"])
 }
 
 dependencies {
